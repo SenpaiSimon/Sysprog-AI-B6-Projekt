04ccbaa9749c56e3869ea091eae62db72c34dcd1
SenpaiSimon <cosmonius.lp.so@googlemail.com>
2022-05-04 19:56:04 +0200

    Comments and moved IOCT Commands to header

--- src/user_app/app.h
+++ src/user_app/app.h
@@ -1,16 +1,18 @@
 #ifndef APP_H
 #define APP_H
 
-// Includes
+/********************************************************************************
+ * Includes from external Librarys
+ ********************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
 
-// Defines
-#define CMD_1 _IOW('a', 'a', int32_t)
-#define CMD_2 _IOW('a', 'b', int32_t)
-#define CMD_3 _IOW('a', 'c', int32_t)
+/********************************************************************************
+ * Includes from internal Librarys
+ ********************************************************************************/
+#include "../kernel_driver/ioct_cmd.h"
 
 #endif
