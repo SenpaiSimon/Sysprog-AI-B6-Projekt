529e90e069a3f2bcb8841b61ab8cbcab2968e8df
SenpaiSimon <cosmonius.lp.so@googlemail.com>
2022-05-12 10:05:46 +0200

    Changed logs dir

--- src/install/install_compile.sh
+++ src/install/install_compile.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
 
-rm -rf log.txt
+rm -rf ./logs/log.txt
 
 # Kernel Module Installation and Log
 echo "Starting compilation of the Kernel Module and installing it..."
-echo "Kernel Install Log" >> log.txt
-echo "" >> log.txt
+echo "Kernel Install Log" >> ./logs/log.txt
+echo "" >> ./logs/log.txt
 
 cd ../kernel_driver
 sudo rmmod led_matrix_driver
-sudo make install >> ../install/log.txt
+sudo make install >> ../install/logs/log.txt
 
 echo "Kernel Module compiled and installed"
 echo ""
 
 
 # App Compilation and Execution
-echo "" >> ../install/log.txt
-echo "App compile Log" >> ../install/log.txt
-echo "" >> ../install/log.txt
+echo "" >> ../install/logs/log.txt
+echo "App compile Log" >> ../install/logs/log.txt
+echo "" >> ../install/logs/log.txt
 
 echo "Compiling User app"
 cd ../user_app
-gcc app.c functions/functions.c led_controller/controller.c -o app >> ../install/log.txt
+gcc app.c functions/functions.c led_controller/controller.c -o app >> ../install/logs/log.txt
 
 echo "Starting user App"
 ./app
--- src/install/start_camera_feed.sh
+++ src/install/start_camera_feed.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 hostname=$(hostname)
 ip=$(hostname -I)
 ip=${ip::-1}
@@ -9,6 +11,6 @@ echo "rtsp://$hostname:8080/"
 echo "or"
 echo "rtsp://$ip:8080/"
 
-rm -rf cameraLog.txt
-touch cameraLog.txt
-raspivid -o - -t 0 -w 1024 -h 768 -fps 30 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8080/}' :demux=h264 &> cameraLog.txt
+rm -rf ./logs/cameraLog.txt
+touch ./logs/cameraLog.txt
+raspivid -o - -t 0 -w 1024 -h 768 -fps 30 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8080/}' :demux=h264 &> ./logs/cameraLog.txt
